96592c51f1622b73966ef5688dd4eee3cb89bf96
DaniOrtegaB <danielortegaberja@gmail.com>
2021-11-16 22:20:12 +0100

    Updated Readme, whitepaper and other files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # RIDS
+
+Motivated by the continuous evolution in electronic device attack techniques and the sophistication they achieve, this research is based on being able to report unwanted modifications that an attacker can make to the most important part of an operating system, the kernel. Among the objectives of the project will aim that this detection code does not have a large impact on the performance of the equipment, adapting to existing workloads and that it is applicable to the market of HP printers. The latest protection measures such as the kernel page table isolation will be taken into account and the architecture of the page tables will be used to detect any attempt to modify their memory space, placing this project among the few which attempt to mitigate kernel vulnerabilities on devices that integrate an updated version of Linux.
+
+## Launch RIDS and bareflank hypervisor  
+`  
+make driver_quick  
+make start
+`  
+## Enjoy :)
--- hypervisor/examples/lids/vmm/main.cpp
+++ hypervisor/examples/lids/vmm/main.cpp
@@ -1,23 +1,4 @@
-//
-// Copyright (C) 2019 Assured Information Security, Inc.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+
 //cmake ../hypervisor/ -DDEFAULT_VMM=lids
 
 #include <vmm.h>
@@ -27,6 +8,7 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <map>
 
 using phys_addr_t = uintptr_t; ///< Phys Address Type (as Int)
 using virt_addr_t = uintptr_t;
@@ -38,13 +20,31 @@ using entry_type = uintptr_t; ///< Entry Type
 using index_type = std::ptrdiff_t;
 
 std::vector<entry_type> ESKernel;
+std::map<uint64_t,std::vector<bool>> ESKernelmap;
+std::vector<entry_type> potentialWarnings;
+
 bool allowWalk = true; //in near future will be an object with the entire state
+bool onlyonetest=true;
+
 
 void global_init()
 {
     bfdebug_info(0, "running LIDS example");
     bfdebug_lnbr(0);
 }
+void testPotentialWarnings(uint64_t dir){
+    uint64_t dir1 = dir;
+    std::vector<bool> permis;
+    permis.push_back(true); //R/W
+    permis.push_back(true); //USER -> Potential warning due to a change in permissions
+    permis.push_back(true); //X 
+
+    ESKernelmap.insert(std::pair<uint64_t,std::vector<bool>>(dir1,permis) );
+    //we introduce hardcoded potential warning to see if detects
+
+
+}
+
 const char *hex_char_to_bin(char c)
 {
     // TODO handle default / error
@@ -173,6 +173,10 @@ void printPTE(entry_type entryOfMap_)
     {
         bfdebug_info(0, "NOT GLOBAL");
     }
+
+    //Here we can do analysis or even save all permissions, now only restore Accesed bit
+    x64::pt::entry::accessed::enable(entryOfMap_);
+
 }
 
 void listESKernelCode(uint64_t entry_)
@@ -181,51 +185,67 @@ void listESKernelCode(uint64_t entry_)
     bool us = x64::pt::entry::us::is_enabled(entry_); //check for kernel supervisor
     bool rw = x64::pt::entry::rw::is_enabled(entry_); // is RO
     bool xd = x64::pt::entry::xd::is_enabled(entry_); //only exectuable
-    if (!rw && !us && !xd)
-    {
-        ESKernel.push_back(entry_);
-        //here goes a map(key, value) where key is entry and value=[rw,us,xd]
-    }
-    std::clog << std::hex << entry_ << " PTE ";
-    if (rw)
-    {
-        std::clog << " R/W ";
-    }
-    else
-    {
-        std::clog << " RO ";
-    }
-    if (us)
-    {
-        std::clog << " US ";
-    }
-    else
-    {
-        std::clog << " S ";
-    }
-    if (xd)
-    {
-        std::clog << " X ";
-    }
-    else
-    {
-        std::clog << " NX ";
-    }
 
-    std::clog << "\n";
+    std::vector<bool> permissions;
+    permissions.push_back(us);
+    permissions.push_back(rw);
+    permissions.push_back(xd);
+
+    if(onlyonetest){
+        testPotentialWarnings(entry_);
+        onlyonetest=false;
+    }
+    if(ESKernelmap.count(entry_)>0){
+        if(ESKernelmap.at(entry_)!=permissions){
+            potentialWarnings.push_back(entry_); //for future implementations this could also save the permissions
+        }
+    }else{
+        ESKernelmap.insert(std::pair<uint64_t,std::vector<bool>>(entry_,permissions) );
+        if (!rw && !us && !xd)
+        {   //those are kernel, exetubale and read only pages
+            ESKernel.push_back(entry_);
+            //here goes a map(key, value) where key is entry and value=[rw,us,xd]
+        }
+        std::clog << std::hex << entry_ << " PTE ";
+        if (rw)
+        {
+            std::clog << " R/W ";
+        }
+        else
+        {
+            std::clog << " RO ";
+        }
+        if (us)
+        {
+            std::clog << " US ";
+        }
+        else
+        {
+            std::clog << " S ";
+        }
+        if (xd)
+        {
+            std::clog << " X ";
+        }
+        else
+        {
+            std::clog << " NX ";
+        }
+
+        std::clog << "\n";
+        }
+
 }
 void walkPT()
 {
-    //bfignored(vcpu);
+    //this function contains lots of commented code, it can be use to track each level of walk paging and see what's going on
     bfdebug_info(0, "Init CR3 walkthrough: ");
 
     entry_type entryOfMap;
     virt_addr_t pgdPointer;
 
-    //    constexpr const auto KERNEL_START = 0xFFFF800000000000;
     uint64_t KERNEL_START = 0xFFFF800000000000;
     uint64_t KERNEL_FINSH = 0xFFFFc87fffffffff;
-    //auto KERNEL_START = 0x1000800000000000;
     mmap *cr3Mmap = vmm_cr3();
     uintptr_t cr3PhysicalAddr = cr3Mmap->cr3();
     uint64_t cr3_Phys = reinterpret_cast<uint64_t>(cr3PhysicalAddr);
@@ -239,65 +259,53 @@ void walkPT()
     bfdebug_nhex(0, "LINEAR ADDRESS:", cr3VirtAdress);
     //auto dest = (cr3VirtAdress>>12) & 0xFFFFFFFFF; //Cogera a partir de los 12 ultimos bits, i aplicara una mascara hasta los 51
 
-    //CR3 stores phys direction where pgd is
-    //but now always are equivalent:
-    //At least since Linux 2.6, pgd and cr3 may or may not be equivalent depending on two factors:
-    //Whether pgd is larger than the virtual base address of the kernel image __START_KERNEL_map.
-    //phys_base, which is the difference between the compile-time physical base address of the kernel image and the run-time physical base address of the image. If the image has been relocated, phys_base would not be zero.
-    //The translation process is performed by a function called __phys_addr which you can refer to to follow the following examples.
-    //Source: https://stackoverflow.com/questions/54973030/difference-between-cr3-value-and-pgd-t
+    int nextpage=0;
+    while(nextpage<2){
 
-    //while(counter<1000){
-    bfdebug_info(0, "--------------------START--------------------");
-    bfdebug_nhex(0, "START_KERNEL Linear Address:", KERNEL_START);
+    
+        //bfdebug_info(0, "--------------------START--------------------");
+        bfdebug_nhex(0, "START_KERNEL Linear Address:", KERNEL_START+nextpage);
 
-    std::vector<uint64_t> pa_PML4s;
-    std::vector<uint64_t> pa_pdptes;
-    std::vector<uint64_t> pa_pds;
-    std::vector<uint64_t> pa_ptes;
+        std::vector<uint64_t> pa_PML4s;
+        std::vector<uint64_t> pa_pdptes;
+        std::vector<uint64_t> pa_pds;
+        std::vector<uint64_t> pa_ptes;
 
-    //phys_addr_t pa_pml4e = (cr3PhysicalAddr & 0xFFFFFFFFFF000) + ((KERNEL_START >> 39) & 0x1FF); //Where 1FF is 9 bits (47:39)
-    //acces content of phys to get virt
-    // std::fstream pml4_file;
-    //pml4_file.open("/home/daniel/Desktop/Bareflank_LIDS/pml4Dump.txt");
-    /*if(!pml4_file){
-            std::clog<<"FILE NOT CREATED";
-        }*/
-    std::clog << "-----------------------------PML4------------------------------ \n";
 
-    uint64_t pa_pml4 = cr3PhysicalAddr;
-    uint64_t *virtPml4 = reinterpret_cast<uint64_t *>(cr3Mmap->cr3Virt().data()); //casteo a puntero
-    //int PML4i = x64::pml4::index(virtPml4); //same as mask ((KERNEL_START >> 39) & 0x1FF);
-    int PML4i = x64::pml4::index(KERNEL_START);
-    uint64_t pa_pml4e;
+        //std::clog << "-----------------------------PML4------------------------------ \n";
 
-    for (int i = 0; i < x64::pml4::num_entries; i++)
-    {
-        uint64_t pml4E = virtPml4[i];
-        if (PML4i == i)
+        uint64_t pa_pml4 = cr3PhysicalAddr;
+        uint64_t *virtPml4 = reinterpret_cast<uint64_t *>(cr3Mmap->cr3Virt().data()); //casteo a puntero
+        //int PML4i = x64::pml4::index(virtPml4); //same as mask ((KERNEL_START >> 39) & 0x1FF);
+        int PML4i = x64::pml4::index(KERNEL_START);
+        uint64_t pa_pml4e;
+
+        for (int i = 0; i < x64::pml4::num_entries; i++)
         {
-            pa_pml4e=pml4E;
-            //pml4_file << "KERNEL Virt PML4e " <<std::dec << i;
-            std::clog << "KERNEL INDEX ";
+            uint64_t pml4E = virtPml4[i];
+            if (PML4i == i)
+            {
+                pa_pml4e=pml4E;
+                //pml4_file << "KERNEL Virt PML4e " <<std::dec << i;
+                //std::clog << "KERNEL INDEX ";
 
-        }else{
-                //pml4_file << "Virt PML4e "  <<std::dec << i;
-                //std::clog << "Virt PML4e "  <<std::dec << i;
+            }else{
+                    //pml4_file << "Virt PML4e "  <<std::dec << i;
+                    //std::clog << "Virt PML4e "  <<std::dec << i;
+            }
+            //pml4_file << std::hex <<  ": 0x" << pml4E << "\n";
+
+            if (pml4E != 0)
+            { //this condition is only for testing, it should be the same as the PML4i
+                pa_PML4s.push_back(pml4E);
+                //std::clog << "Virt PML4e " << std::dec << i;
+                //std::clog << std::hex << ": 0x" << pml4E << "\n";
+            }
         }
-        //pml4_file << std::hex <<  ": 0x" << pml4E << "\n";
 
-        if (pml4E != 0)
-        { //this condition is only for testing, it should be the same as the PML4i
-            pa_PML4s.push_back(pml4E);
-            std::clog << "Virt PML4e " << std::dec << i;
-            std::clog << std::hex << ": 0x" << pml4E << "\n";
-        }
-    }
+        //std::clog << "Target PML4 Offset is: "<< std::dec << PML4i <<"\n";
 
-    //pml4_file << "Target PML4 Offset is: "<< std::dec << PML4i <<"\n";
-    std::clog << "Target PML4 Offset is: "<< std::dec << PML4i <<"\n";
-
-        std::clog<<"-----------------------------PDPT------------------------------ \n";
+        //std::clog<<"-----------------------------PDPT------------------------------ \n";
         //pml4_file.close();//make dump>
         
         for(uint64_t pa_pml4e : pa_PML4s ){
@@ -309,7 +317,7 @@ void walkPT()
             for(int i=0;i<x64::pdpt::num_entries;i++){
                 uint64_t PDPTe = virtPdpt[i];
                 if(PDPTi==i){
-                    std::clog << "KERNEL INDEX ";
+                    //std::clog << "KERNEL INDEX ";
 
                 }/*else{
                     std::clog << "Virt PDPTe "  <<std::dec << i;
@@ -317,16 +325,16 @@ void walkPT()
                 //std::clog << std::hex <<  ": 0x" << PDPTe << "\n";
                 if(PDPTe!=0){
                     pa_pdptes.push_back(PDPTe);
-                    std::clog << "Virt PDPTe "  <<std::dec << i;
-                    std::clog << std::hex <<  ": 0x" << PDPTe << "\n";
+                    //std::clog << "Virt PDPTe "  <<std::dec << i;
+                    //std::clog << std::hex <<  ": 0x" << PDPTe << "\n";
                 }
             }
-            std::clog<< "Target PDPT Offset is: "<< std::dec << PDPTi <<"\n";
+            //std::clog<< "Target PDPT Offset is: "<< std::dec << PDPTi <<"\n";
         }
 
         
         
-        std::clog<<"-----------------------------PD------------------------------ \n";
+        //std::clog<<"-----------------------------PD------------------------------ \n";
 
         for(uint64_t pa_pdpte : pa_pdptes ){
 
@@ -337,21 +345,21 @@ void walkPT()
             for(int i=0;i<x64::pd::num_entries;i++){
                 uint64_t PDe = virtPd[i];
                 if(PDi==i){
-                    std::clog << "KERNEL INDEX ";
+                    //std::clog << "KERNEL INDEX ";
                 }
                 /*}else{
                     std::clog << "Virt PDe "  <<std::dec << i;
                 }*/
                 if(PDe!=0){
                     pa_pds.push_back(PDe);
-                    std::clog << "Virt PDe "  <<std::dec << i;
-                    std::clog << std::hex <<  ": 0x" << PDe << "\n";
+                    //std::clog << "Virt PDe "  <<std::dec << i;
+                    //std::clog << std::hex <<  ": 0x" << PDe << "\n";
                 }
 
                
             }
 
-            std::clog<< "Target PD Offset is: "<< std::dec << PDi <<"\n";
+            //std::clog<< "Target PD Offset is: "<< std::dec << PDi <<"\n";
         }
 
         for(uint64_t pa_pde : pa_pds ){
@@ -362,60 +370,73 @@ void walkPT()
             for(int i=0;i<x64::pt::num_entries;i++){
                 uint64_t PTe = virtPt[i];
                 if(PTi==i){
-                    std::clog << "KERNEL INDEX: ";
+                    //std::clog << "KERNEL INDEX: ";
                 }/*else{
                     std::clog << "Virt PTe "  <<std::dec << i;
                 }*/
                 if(PTe!=0){
                     pa_ptes.push_back(PTe);
-                    std::clog << "Virt PDe "  <<std::dec << i;
-                    std::clog << std::hex <<  ": 0x" << PTe << "\n";
+                    //std::clog << "Virt PDe "  <<std::dec << i;
+                    //std::clog << std::hex <<  ": 0x" << PTe << "\n";
 
                 }
             }
 
-            std::clog<< "Target PT Offset is: "<< std::dec << PTi <<"\n";
+            //std::clog<< "Target PT Offset is: "<< std::dec << PTi <<"\n";
         }
 
         for (uint64_t pte : pa_ptes){
             //printPTE(pte);
             listESKernelCode(pte); //will save X,RO, supervisor
         }
-        //listESKernelCode(PTe);
 
 
         
 
+        nextpage++;
 
-        //bfdebug_ndec(0,"Counter",counter);
-        bfdebug_info(0,"--------------------FINISH--------------------");
+        //bfdebug_info(0,"--------------------FINISH--------------------");
 
-        //printPTE(entryPte.first);
-        //KERNEL_START+=4095; //FFF that is offset page
-        //counter++;
     
-    //}
-    
-    /*for(long i=0;i< ESKernel.size();i++)
-    {
-        bfdebug_nhex(0,"ES Page: ",ESKernel[i]);
     }
-    bfdebug_ndec(0,"Executable Suppervisor PTE's: ", ESKernel.size());
+    
+}
 
-     */
+void printPotentialWarnings(){
+    std::clog << "Potential Warnings: "<< std::endl ;
+
+    for(uintptr_t warning : potentialWarnings ){
+        std::clog << std::hex << warning;
+    }
+
+}
+bool cr3_handler(vcpu_t *vcpu){
+    /*auto &d = vcpu->data<d_t &>();
+    
+    bfdebug_nhex(0,"CR3: ", vcpu->guest_cr3());
+    return true;*/
+}
+void getCR3(vcpu_t *vcpu){
+    //map phys addres, see unique_map logic
+    // to get cr3 kernel, watch cr3 exits, clear TLB etc
+    // 
+    vcpu->add_wrcr3_handler(cr3_handler);
+    
 }
 void vcpu_init_nonroot(vcpu_t *vcpu)
 {
-    //vcpu->dump("Thats the state dump");
     int i = 0;
 
     while (allowWalk)
     {
-        if (i == 1)
+       if (i == 0)
         {
-            allowWalk = false;
+           allowWalk = false;
         }
         walkPT();
-        i++;
+       i++;
     }
+
+    printPotentialWarnings();
+    //getCR3(vcpu);
 }
